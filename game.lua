48e0971ce1de3a26200d1a0afbd1a7ff9088d8ad
Westerbly Snaydley <westerbly@gmail.com>
2020-10-22 10:27:06 -0500

    added camera

--- game.lua
+++ game.lua
@@ -49,7 +49,7 @@ local function chk_spr_map(obj,x,y)
 end
 
 local function chk_spr_spr(obj1,obj2,x,y)
-    local f1,f2 = peek(0x14404+obj1.spr), peek(0x14404+obj2.spr)
+    local f1,f2 = parse_flags(peek(0x14404+obj1.spr)), parse_flags(peek(0x14404+obj2.spr))
 end
 
 ents = {}
@@ -79,8 +79,31 @@ end
 local plr = ent("player",0,0,2,2,0,256)
 local max_hook_len, hook_len, is_hooking = 80,0,false
 
+-- CAMERA
+-- ======
+cam = {
+    x=0,
+    y=0
+}
+
+function cam:follow(x, y)
+    self.x,self.y = x-(240/2),y-(136/2)
+end
+function cam:to_screen(x, y)
+    return x-self.x, y-self.y
+end
+
+local function draw_map()
+    local cx,cy = cam.x,cam.y
+    local mx, my = cx//8,cy//8
+    map(mx,my,30+1,17+1,mx*8-cx,my*8-cy,0,1)
+end
+
 function TIC ()
-    cls()
+    cls(0)
+    cam:follow(plr.x, plr.y)
+    draw_map()
+
     print(fmt("0x%x", (peek(0x14404 + 1)&0xF0)>>4))
     t = t+1
 end
